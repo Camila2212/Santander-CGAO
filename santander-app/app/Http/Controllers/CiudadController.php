def3d774babfad0045a782a68dc7611990e7e4a0
DESKTOP-ASJ6OGH\SENA <camilagonmar2212@gmail.com>
2023-09-27 10:18:45 -0500

    primera prueba

---
+++ santander-app/app/Http/Controllers/CiudadController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CiudadController extends Controller
+{
+     
+    public function index(){
+        $datos=DB::select('select * from ciudades');
+        return view('ciudad')->with('datos',$datos);
+
+    }
+    public function create(Request $request){
+        try
+        {
+            $sql=DB::insert('insert into ciudades(`idCiudad`, `nombreCiudad`)
+            values(Null,?,?)');
+        }
+        catch (\Throwable $e)
+        {
+            
+        }
+    }
+}
