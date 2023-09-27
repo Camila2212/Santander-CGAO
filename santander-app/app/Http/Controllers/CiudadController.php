71ef0f1572cc70b21ab251896e6234df96567ecf
DESKTOP-ASJ6OGH\SENA <camilagonmar2212@gmail.com>
2023-09-27 10:28:07 -0500

    env

--- santander-app/app/Http/Controllers/CiudadController.php
+++ santander-app/app/Http/Controllers/CiudadController.php
@@ -6,21 +6,5 @@ use Illuminate\Http\Request;
 
 class CiudadController extends Controller
 {
-     
-    public function index(){
-        $datos=DB::select('select * from ciudades');
-        return view('ciudad')->with('datos',$datos);
-
-    }
-    public function create(Request $request){
-        try
-        {
-            $sql=DB::insert('insert into ciudades(`idCiudad`, `nombreCiudad`)
-            values(Null,?,?)');
-        }
-        catch (\Throwable $e)
-        {
-            
-        }
-    }
+    //
 }
